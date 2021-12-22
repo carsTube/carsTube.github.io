306686869689f2ac185fe0f0e1035838647845b5
YuTaral <yutaral99@abv.bg>
2021-12-22 10:49:30 +0200

    implemented profile page

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,17 +1,22 @@
 import { getAds } from '../data/data.js';
 import { html, until } from '../lib.js';
 import { getUserData } from '../util.js';
-import { loadAds } from './catalog.js';
-import { spinner } from '../middlewares.js';
+import { adCard, loadAds } from './catalog.js';
+import { createErrorModal } from '../middlewares.js';
 
-const profileTemplate = (adsPromise, userData) => html`
+
+const profileTemplate = (ads, userData) => html`
 <section id="profile">
     <div class="hero">
         <span>Greetings ${userData.username}</span>
         <span>Your email: ${userData.email}</span> 
+        <span>Your published ads: ${ads.length}</span> 
     </div>
-    <div class="hero"><h3>Your ads:</h3></div>
-    ${until(adsPromise, spinner())}
+    
+    ${ads.length == 0 ? html `${createErrorModal('You dont have any ads. Go to Create Ad to publish new Ads.')}` : 
+    html`<div class="hero"><h3>Your ads:</h3></div>
+    ${ads.map(adCard)}`}
+    
     <footer><div class="hero"><a class="actionLink" href="/home">Go back to Home page</a></div></footer>
 </section>
 `
@@ -19,7 +24,7 @@ const profileTemplate = (adsPromise, userData) => html`
 
 export async function profilePage(ctx) {
     const userData = getUserData();
-    const adsPromise = getAds(1, '', false, true);
-    ctx.render(profileTemplate(loadAds(adsPromise), userData));
+    const ads = await getAds(1, '', false, true);
+    ctx.render(profileTemplate(ads.results, userData));
 }
 
